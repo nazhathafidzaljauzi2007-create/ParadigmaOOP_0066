b767ce56b6b7004d7ab90be5893c894fce27d5c1
nazhat <nazhathafidzaljauzi2007@gmail.com>
2026-04-21 15:21:08 +0700

    Update OOP.cpp

--- OOP.cpp
+++ OOP.cpp
@@ -7,7 +7,7 @@ class Mahasiswa
         int nim;
         string nama; 
         float nilai;
-        void printData() 
+        void printData()  
         {
             cout << "NIM = " << nim << endl;
             cout << "Nama = " << nama << endl;
